4bbd8077fb6b51bc87d750345227d446fa49f80a
Santiago <thekamarovyt@gmail.com>
2020-03-12 21:03:45 -0300

    agregue el ignore

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Inicio del programa Clase_2020_12_13");
 	// write your code here
     }
 }
